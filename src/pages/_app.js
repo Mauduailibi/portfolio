215c064ae7ee6c5653b6901e55582ca95ee770e2
Mauricio Duailibi Neto <maudneto@gmail.com>
2023-08-11 23:30:39 -0300

    GTM fixed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,7 +5,8 @@ import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import { AnimatePresence } from 'framer-motion';
 import { useRouter } from 'next/router';
-import Script from 'next/script';
+import { useEffect } from 'react';
+import TagManager from 'react-gtm-module';
 
 const montserrat = Montserrat({
   subsets: ['latin'],
@@ -15,24 +16,15 @@ const montserrat = Montserrat({
 export default function App({ Component, pageProps }) {
   const router = useRouter();
 
+  useEffect(() => {
+    TagManager.initialize({ gtmId: process.env.GA_MEASUREMENT_ID });
+  }, []);
+
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
-        {/* Google tag (gtag.js) */}
-        <Script
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_MEASUREMENT_ID}`}
-        />
-        <Script id="google-analytics">
-          {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
- 
-		  gtag('config', ${process.env.GA_MEASUREMENT_ID});
-        `}
-        </Script>
       </Head>
       <main
         className={`${montserrat.variable} font-mont bg-light w-full min-h-screen dark:bg-dark`}
